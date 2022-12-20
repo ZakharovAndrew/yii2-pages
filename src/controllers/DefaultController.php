d4c517a6fc1ba76f009e5d96693d36d39ab0530d
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-12-20 11:01:31 +0300

    Update DefaultController.php

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -51,6 +51,69 @@ class DefaultController extends Controller
     }
     
     /**
+     * Displays a single Page model.
+     * @param int $id ID
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($url)
+    {
+        return $this->render('view', [
+            'model' => $this->findModelbyUrl($url),
+        ]);
+    }
+    
+    /**
+     * Updates an existing Page model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param int $id ID
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'url' => $model->url]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+    
+    /**
+     * Deletes an existing Page model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param int $id ID
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+    
+    /**
+     * Finds the Pages model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $id ID
+     * @return Page the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Pages::findOne(['id' => $id])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+    
+    /**
      * Finds the Pages model based on its url value.
      * If the model is not found, a 404 HTTP exception will be thrown.
      * @param string $url url
