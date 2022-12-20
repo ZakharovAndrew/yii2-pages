89cc267bcc2c789329f88e359734f8d7e0b67528
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-12-20 10:16:16 +0300

    Init

---
+++ src/Module.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Yii2 Pages
+ * *************
+ * Yii2 pages with database module with GUI manager supported.
+ *  
+ * @link https://github.com/ZakharovAndrew/yii2-pages/
+ * @copyright Copyright (c) 2022 Zakharov Andrew
+ */
+ 
+namespace ZakharovAndrew\settings;
+
+use Yii;
+
+/**
+ * Class Module 
+ */
+class Module extends \yii\base\Module
+{    
+    public $bootstrapVersion = '';
+    
+    /**
+     *
+     * @var string source language for translation 
+     */
+    public $sourceLanguage = 'en-US';
+    
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'ZakharovAndrew\pages\controllers';
+
+    /**
+     * {@inheritdoc}
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+        $this->registerTranslations();
+    }
+    
+    /**
+     * Registers the translation files
+     */
+    protected function registerTranslations()
+    {
+        Yii::$app->i18n->translations['extension/yii2-settings/*'] = [
+            'class' => 'yii\i18n\PhpMessageSource',
+            'sourceLanguage' => $this->sourceLanguage,
+            'basePath' => '@vendor/zakharov-andrew/yii2-pages/src/messages',
+            'on missingTranslation' => ['app\components\TranslationEventHandler', 'handleMissingTranslation'],
+            'fileMap' => [
+                'extension/yii2-pages/settings' => 'settings.php',
+            ],
+        ];
+    }
+
+    /**
+     * Translates a message. This is just a wrapper of Yii::t
+     *
+     * @see Yii::t
+     *
+     * @param $category
+     * @param $message
+     * @param array $params
+     * @param null $language
+     * @return string
+     */
+    public static function t($message, $params = [], $language = null)
+    {
+        $category = 'settings';
+        return Yii::t('extension/yii2-pages/' . $category, $message, $params, $language);
+    }
+    
+}
