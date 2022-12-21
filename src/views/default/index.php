75905f0789d0fe6065cb88489242fd51857a7ab9
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-12-21 13:09:24 +0300

    Add files via upload

---
+++ src/views/default/index.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\PageSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Страницы';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="page-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Создать страницу', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'title',
+            'url:url',
+            'content:ntext',
+            'datetime_at',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
