cbb2b545dd692a8c60d5cd56754aa1c75927113b
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-12-21 13:33:04 +0300

    Update update.php

--- src/views/default/update.php
+++ src/views/default/update.php
@@ -3,16 +3,16 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\Page */
+/* @var $searchModel ZakharovAndrew\pages\models\Pages */
 
 $this->title = 'Обновить страницу: ' . $model->title;
 $this->params['breadcrumbs'][] = ['label' => 'Страницы', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
 ?>
-<div class="page-update">
+<div class="pages-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <div class="alert alert-info">Можно использовать текст <b>[category_list]12[/category_list]</b> для вставки 6 товаров заданной картегории. Категория задается цифрой</div>
+
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
