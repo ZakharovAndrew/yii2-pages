1f0b63deccaa48c7c8599e41f9e3a2c324902581
ZakharovAndrew <37038505+ZakharovAndrew@users.noreply.github.com>
2022-12-22 12:34:27 +0300

    Update Pages.php

--- src/models/Pages.php
+++ src/models/Pages.php
@@ -3,6 +3,7 @@
 namespace ZakharovAndrew\pages\models;
 
 use Yii;
+use ZakharovAndrew\pages\Module;
 
 /**
  * This is the model class for table "pages".
@@ -43,11 +44,45 @@ class Pages extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'title' => 'Заголовок',
+            'title' => Module::t('Title'),
             'url' => 'url',
-            'content' => 'Content',
+            'content' => Module::t('Content'),
             'create_at' => 'Create At',
         ];
+    }
+    
+    /**
+     * Schema for translite
+     * @return array
+     */
+    public static function getSchema()
+    {
+        return [
+            "Є"=>"EH","І"=>"I","і"=>"i","№"=>"#","є"=>"eh",
+            "А"=>"A","Б"=>"B","В"=>"V","Г"=>"G","Д"=>"D",
+            "Е"=>"E","Ё"=>"JO","Ж"=>"ZH",
+            "З"=>"Z","И"=>"I","Й"=>"JJ","К"=>"K","Л"=>"L",
+            "М"=>"M","Н"=>"N","О"=>"O","П"=>"P","Р"=>"R",
+            "С"=>"S","Т"=>"T","У"=>"U","Ф"=>"F","Х"=>"KH",
+            "Ц"=>"C","Ч"=>"CH","Ш"=>"SH","Щ"=>"SHH","Ъ"=>"'",
+            "Ы"=>"Y","Ь"=>"","Э"=>"EH","Ю"=>"YU","Я"=>"YA",
+            "а"=>"a","б"=>"b","в"=>"v","г"=>"g","д"=>"d",
+            "е"=>"e","ё"=>"jo","ж"=>"zh",
+            "з"=>"z","и"=>"i","й"=>"jj","к"=>"k","л"=>"l",
+            "м"=>"m","н"=>"n","о"=>"o","п"=>"p","р"=>"r",
+            "с"=>"s","т"=>"t","у"=>"u","ф"=>"f","х"=>"kh",
+            "ц"=>"c","ч"=>"ch","ш"=>"sh","щ"=>"shh","ъ"=>"",
+            "ы"=>"y","ь"=>"","э"=>"eh","ю"=>"yu","я"=>"ya","«"=>"","»"=>"","—"=>"-"," "=>"-",','=>''
+        ];
+    }
+    
+    /**
+     * Транслетиризация
+     * @return string
+     */
+    public static function translite($text)
+    {
+        return strtr(mb_strtolower($text), static::getSchema());
     }
     
 }
